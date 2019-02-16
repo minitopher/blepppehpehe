07638eadd0fee6f59b672ed8d371bb43a911f110
minitopher <minitopher@gmail.com>
2019-02-16 14:56:00 -0500

    Added an array of fortunes

--- Learning Git/src/hub.java
+++ Learning Git/src/hub.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class hub {
 	static String name;
 	static Scanner sc = new Scanner(System.in);
+	static String[] fortunes= {"Your future will have many ups and downs","You are doomed to die tomorrow", "", "" ,"" };
 	
 	
 	public static void name(){
 		System.out.println("Enter your age name please:");
 		name = sc.nextLine();
+		
 		if (name.length() > 5){
 			System.out.println("Your future will have many ups and downs");
-		}else{
-			System.out.println("");
+		}else if (name.length() <= 5){
+			System.out.println("You are doomed to die tomorrow");
 		}
 	}
 
